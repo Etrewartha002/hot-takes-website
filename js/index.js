7e9199ae6281752ddd08cfd25def9390c5cd204f
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-12-05 17:46:42 -0600

    Added some code
    Added a loading screen, unfinished and doesn't actually load

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+function loadContent(){
+    //Load Twelve hot takes
+}
 document.querySelector("button").addEventListener("click", ()=>{
-    console.log("working")
+    console.log("working");
     document.querySelector("button").setAttribute("id", "go-button");
     setTimeout(()=>{
         console.log("working")
         document.querySelector("button").removeAttribute("id");
     }, 100);
+    loadContent().then()
 })
