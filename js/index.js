12b91975d8c852b77d1cccc88d39402fc16f6f76
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-12-11 07:31:01 -0600

    Useless changes that will probably change

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-
+function loadContent(callback){
+    //Load Twelve hot takes
+    callback();
+}
+function displayData(){
+    //display the hot takes
+    //remove the loading page and all of
+}
 document.querySelector(".go-button").addEventListener("click", ()=>{
     console.log("working1");
     document.querySelector("button").setAttribute("id", "go-button");
--- js/react-app.js
+++ js/react-app.js
@@ -1,11 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function loadContent(callback){
-    //Load Twelve hot takes
-    callback();
-}
-function displayData(){
-    //display the hot takes
-    //
-}
